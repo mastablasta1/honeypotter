c97638a3acadb85de36dc566afc90f825da84db5
Tomasz Idziak <tomasz.idziak@gmail.com>
2016-04-30 22:17:29 +0200

    Destroy function added

--- manage_honeypot.bat
+++ manage_honeypot.bat
@@ -1,17 +1,22 @@
 @echo off
+setlocal
 
 ::--- Parameters
+set MANAGEMENT_PASSWORD=management
 set VM_DIR_NAME=honeypot_data
-set VAGRANT_BOX=puppetlabs/ubuntu-14.04-64-puppet
+set VAGRANT_BOX=ubuntu/trusty64
+::set VAGRANT_BOX=puppetlabs/ubuntu-14.04-64-puppet
 ::--- End of parameters
 
-
+set ERRORLEVEL=
 set ROOT_DIR=%~dp0
 set ROOT_DIR=%ROOT_DIR:~0,-1%
 set VM_DIR=%ROOT_DIR%\%VM_DIR_NAME%
 set VAGRANT_FILE_PATH=%VM_DIR%\Vagrantfile
 set VAGRANT_METADATA_DIR=%VM_DIR%\.vagrant
 set VAGRANT_SNAPSHOTS_DIR=%VM_DIR%\snapshots
+set PUPPET_MANIFESTS_DIR=%VM_DIR%\manifests
+set PUPPET_MANIFESTS_FILE=%PUPPET_MANIFESTS_DIR%\default.pp
 
 echo Honeypotter 0.1
 echo.
@@ -30,9 +35,18 @@ if %ERRORLEVEL% NEQ 0 (
 	exit /b 1
 )
 
-if "%1"=="help" call :help
-if "%1"=="create" call :create
-if "%1"=="delete" call :delete
+if "%1"=="help" ( 
+	call :help
+) else if "%1"=="create" ( 
+	call :create
+) else if "%1"=="destroy" ( 
+	call :destroy
+) else if "%1"=="start" ( 
+	call :start
+) else (
+	set ERRORLEVEL=2
+	echo No such command. Try '%~n0 help'.
+)
 
 exit /b %ERRORLEVEL%
 
@@ -48,10 +62,12 @@ mkdir %VM_DIR%
 if errorlevel 1 exit /b %ERRORLEVEL%
 mkdir %VAGRANT_SNAPSHOTS_DIR%
 if errorlevel 1 exit /b %ERRORLEVEL%
+mkdir %PUPPET_MANIFESTS_DIR%
+if errorlevel 1 exit /b %ERRORLEVEL%
 
 ::--- Check if VM was already created
 if exist %VAGRANT_METADATA_DIR% (
-	echo Honeypot was already created. To create new honeypot, run "delete_honeypot.bat" first.
+	echo Honeypot was already created. To create new honeypot, run "delete" command first.
 	exit /b 2
 )
 
@@ -71,12 +87,37 @@ if exist %VAGRANT_METADATA_DIR% (
 	echo             mkdir -p /etc/puppet/modules;
 	echo             puppet module install puppetlabs-apache;
 	echo             puppet module install puppetlabs-mysql"
-	rem echo   config.vm.provision "puppet" do ^|puppet^|
-	rem echo     puppet.manifests_path = "manifests"
-	rem echo     puppet.manifest_file = "default.pp"
-	rem echo   end
+	echo   config.vm.provision "puppet" do ^|puppet^|
+	echo     puppet.manifests_path = "manifests"
+	echo     puppet.manifest_file = "default.pp"
+	echo   end
 	echo end
 ) >%VAGRANT_FILE_PATH%
+
+(
+	rem echo exec { 'apt-update':
+	rem echo 	command => '/usr/bin/apt-get update'
+	rem echo }
+	echo.
+	echo package { 'whois':
+	echo 	ensure =^> installed,
+	echo }
+	echo.
+	echo group { 'management':
+	echo 	ensure	=^> present,
+	echo }
+	echo.
+	echo user { 'management':
+	echo 	require	=^> Package['whois'],
+	echo 	ensure	=^> present,
+	echo 	groups	=^> 'management',
+	echo 	shell	=^> '/bin/bash',
+	echo 	password	=^> generate^("mkpasswd -m sha-512 %MANAGEMENT_PASSWORD% | tr -d '\n'"^),
+	rem echo 	password	=^> generate^('/bin/sh', '-c', "mkpasswd -m sha-512 %MANAGEMENT_PASSWORD% | tr -d '\n'"^),
+	echo 	home	=^> '/home/management',
+	echo 	managehome	=^> true
+	echo }
+) >%PUPPET_MANIFESTS_FILE%
 
 echo Created honeypot definition in: %VM_DIR%
 exit /b 0
@@ -87,21 +128,44 @@ exit /b 0
 echo Available commands:
 echo 	create	creates a definition of honeypot in the location of this script
 echo.
-echo 	delete	deletes honeypot existing in a directory
+echo 	destroy	shuts down honeypot and deletes its definition and VM data
 echo.
 echo 	start	starts a honeypot (virtual machine on VirtualBox). Requires an existing honeypot definition.
+echo.
+echo 	stop	stops a running honeypot virtual machine.
 echo.
 echo 	help	displays this help
 exit /b 0
 ::--- END OF HELP FUNCTION
 
-::--- DELETE FUNCTION
-:delete
+::--- START HONEYPOT
+:start
+pushd %VM_DIR%
+vagrant up
+if errorlevel 1 (
+	echo.
+	echo Honeypot start failed.
+)
+popd
+exit /b %ERRORLEVEL%
+::--- END OF START HONEYPOT
+
+::--- STOP HONEYPOT
+:stop
+exit /b 0
+::--- END OF STOP HONEYPOT
+
+::--- DESTROY
+:destroy
 if exist %VM_DIR% (
+	pushd %VM_DIR%
+	vagrant destroy --force
+	popd
+
 	rmdir %VM_DIR% /S /Q
-	if not errorlevel 1 echo Honeypot deleted.
+	if not errorlevel 1 echo Honeypot destroyed.
 	exit /b %ERRORLEVEL%
 )
-echo Honeypot definition does not exist. Nothing to do.
+echo Honeypot does not exist. Nothing to do.
 exit /b 0
-::--- END OF DELETE FUNCTION
+::--- END OF DESTROY
