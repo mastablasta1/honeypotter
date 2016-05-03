052f58488175e2ee451c631dcb659ec8026e25bf
Tomasz Idziak <tomasz.idziak@gmail.com>
2016-05-03 18:41:11 +0200

    shared folder working

--- manage_honeypot.bat
+++ manage_honeypot.bat
@@ -5,7 +5,7 @@
 set MANAGEMENT_ACCOUNT_PASSWORD=secret
 set WEBSERVER_PUBLIC_DOMAIN=blog.honeypotter.org
 set PUBLIC_IP_ADDRESS=192.168.12.21
-set FORWARDED_SSH_PORT=2222
+set FORWARDED_SSH_PORT=2244
 set HONEYPOT_HOSTNAME=webserver
 
 ::--- End of parameters
@@ -15,7 +15,7 @@ set HONEYPOT_HOSTNAME=webserver
 setlocal EnableDelayedExpansion
 
 set ERRORLEVEL=
-set VAGRANT_BOX=ubuntu/trusty64
+set VAGRANT_BOX=ubuntu/wily64
 set ROOT_DIR=%~dp0
 set ROOT_DIR=%ROOT_DIR:~0,-1%
 set VM_DIR_NAME=honeypot_data
@@ -24,8 +24,8 @@ set VAGRANT_FILE_PATH=%VM_DIR%\Vagrantfile
 set VAGRANT_METADATA_DIR=%VM_DIR%\.vagrant
 rem set VAGRANT_SNAPSHOTS_DIR=%VM_DIR%\snapshots
 set PUPPET_MANIFESTS_DIR=%VM_DIR%\manifests
-set LOGS_DIR_NAME=hp_logs
-set LOGS_DIR=%VM_DIR%\hp_logs
+set LOGS_DIR_NAME=logs
+set LOGS_DIR=%VM_DIR%\%LOGS_DIR_NAME%
 set PUPPET_MANIFESTS_FILE=%PUPPET_MANIFESTS_DIR%\default.pp
 set MANAGEMENT_ACCOUNT_NAME=management
 
@@ -78,14 +78,15 @@ mkdir %PUPPET_MANIFESTS_DIR%
 if errorlevel 1 exit /b !ERRORLEVEL!
 mkdir %LOGS_DIR%
 if errorlevel 1 exit /b !ERRORLEVEL!
-rem mkdir %VAGRANT_SNAPSHOTS_DIR%
-rem if errorlevel 1 exit /b !ERRORLEVEL!
+
 
 ::--- Check if VM was already created
 if exist %VAGRANT_METADATA_DIR% (
 	echo Honeypot was already configured in this directory. Run "destroy" command first.
 	exit /b 2
 )
+
+set GUEST_LOGS_DIR=/home/%MANAGEMENT_ACCOUNT_NAME%/%LOGS_DIR_NAME%
 
 (
 	echo Vagrant.configure^(2^) do ^|config^|
@@ -95,25 +96,32 @@ if exist %VAGRANT_METADATA_DIR% (
 	echo   config.vm.hostname = "%HONEYPOT_HOSTNAME%"
 	echo   config.vm.synced_folder ".", "/vagrant", disabled: true
 	rem echo   config.vm.synced_folder "%LOGS_DIR:\=/%/", "/home/%MANAGEMENT_ACCOUNT_NAME%", owner: "%MANAGEMENT_ACCOUNT_NAME%", group: "%MANAGEMENT_ACCOUNT_NAME%"
-	echo   config.vm.network "forwarded_port", guest: 22, host: %FORWARDED_SSH_PORT%
-	echo   config.vm.network "forwarded_port", guest: 80, host: 8080
 	echo   config.vm.network "private_network", ip: "%PUBLIC_IP_ADDRESS%"
+	echo   config.vbguest.auto_update = false
 	echo   config.vm.provider "virtualbox" do ^|vb^|
 	echo     vb.gui = true
-	rem echo     vb.linked_clone = true
+	echo     vb.memory = "1024"
+	echo     vb.cpus = 1
 	rem echo     vb.customize ["modifyvm", :id, "--snapshotfolder", "%VAGRANT_SNAPSHOTS_DIR:\=/%"]
 	echo     vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+	echo     vb.customize ["sharedfolder", "add", :id, "--name", "%LOGS_DIR_NAME%", "--hostpath", "%LOGS_DIR:\=/%"]
 	echo   end
-	echo   config.vm.provision "shell", 
-	echo     inline: "sudo apt-get install whois;
+	echo   config.vm.provision "pre_puppet", type: "shell" do ^|s^|
+	echo     s.inline = "sudo apt-get install whois;
 	echo             wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb;
 	echo             sudo dpkg -i puppetlabs-release-precise.deb;
 	echo             mkdir -p /etc/puppet/modules;
 	echo             puppet module install puppetlabs-apache;
 	echo             puppet module install puppetlabs-mysql"
-	echo   config.vm.provision "puppet" do ^|puppet^|
+	echo   end
+	echo   config.vm.provision "puppet", type: "puppet" do ^|puppet^|
 	echo     puppet.manifests_path = "manifests"
 	echo     puppet.manifest_file = "default.pp"
+	echo   end
+	echo   config.vm.provision "post_puppet", type: "shell" do ^|s^|
+	echo     s.inline = "mkdir %GUEST_LOGS_DIR%;
+	echo                 chown %MANAGEMENT_ACCOUNT_NAME%:%MANAGEMENT_ACCOUNT_NAME% %GUEST_LOGS_DIR%;
+    echo                 mount -t vboxsf -o uid=`id -u %MANAGEMENT_ACCOUNT_NAME%`,gid=`id -g %MANAGEMENT_ACCOUNT_NAME%` %LOGS_DIR_NAME% %GUEST_LOGS_DIR%"
 	echo   end
 	echo end
 ) >%VAGRANT_FILE_PATH%
@@ -132,7 +140,7 @@ if exist %VAGRANT_METADATA_DIR% (
 	echo user { '%MANAGEMENT_ACCOUNT_NAME%':
 	echo 	require	=^> Group['%MANAGEMENT_ACCOUNT_NAME%'],
 	echo 	ensure	=^> present,
-	echo 	groups	=^> ['%MANAGEMENT_ACCOUNT_NAME%','sudo'],
+	echo 	groups	=^> ['%MANAGEMENT_ACCOUNT_NAME%','sudo','vboxsf'],
 	echo 	shell	=^> '/bin/bash',
 	echo 	password	=^> generate^('/bin/bash', '-c', "mkpasswd -m sha-512 %MANAGEMENT_ACCOUNT_PASSWORD% | tr -d '\n'"^),
 	echo 	home	=^> '/home/%MANAGEMENT_ACCOUNT_NAME%',
